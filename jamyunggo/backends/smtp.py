ec65601e9e9a2554fe8c66bc38eb637adf8f180a
Wonsup Yoon <pusnow@me.com>
2018-08-09 17:18:41 +0900

    SMTP From field changes

--- jamyunggo/backends/smtp.py
+++ jamyunggo/backends/smtp.py
@@ -2,6 +2,8 @@ import config
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.utils import formataddr
 
 
 def notify(module_name, title, text=None, url=None):
@@ -21,7 +23,8 @@ def notify(module_name, title, text=None, url=None):
 
         for receivor in config.SMTP_RECEIVERS:
             msg['Subject'] = "[" + module_name + "] " + title
-            msg['From'] = "Jamyunggo"
+            msg['From'] = formataddr((str(Header("Jamyunggo", 'utf-8')),
+                                      config.SMTP_ID))
             msg['To'] = receivor
 
             msg.attach(MIMEText(html, "html"))
