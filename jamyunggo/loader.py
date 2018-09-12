38183fb7dfd6e5c1621e95bc9e03e88a546f8f1c
Wonsup Yoon <pusnow@me.com>
2018-09-12 23:48:49 +0900

    Fix

--- jamyunggo/loader.py
+++ jamyunggo/loader.py
@@ -43,19 +43,19 @@ class Loader:
         self.jamyunggo_module = []
         self.jamyunggo_list = []
 
-        dirs = os.listdir("pages")
+        dirs = [ ("pages", a) for a in os.listdir("pages")]
 
         if os.path.isdir("privates"):
-            dirs += os.listdir("privates")
+            dirs += [ ("privates", a) for a in os.listdir("privates")]
 
-        for py_file in dirs:
+        for super_dir, py_file in dirs:
             if py_file.endswith(".py"):
                 module_name = py_file[:-3]
                 module_path = "jamyunggo.backends." + module_name
                 if module_path in module_names:
                     module = importlib.reload(module_names[module_path])
                 else:
-                    module = importlib.import_module("pages." + module_name)
+                    module = importlib.import_module(super_dir + "." + module_name)
                 self.jamyunggo_module.append(module)
                 headers = module.HEADERS if hasattr(module, "HEADERS") else {}
                 body_url_fn = module.BODY_URL_FN if hasattr(
