763ae9cd4e63574bad07d58a33468d3eba8404d6
Wonsup Yoon <pusnow@me.com>
2018-09-12 23:06:07 +0900

    Remove Telegram photo

--- jamyunggo/backends/telegram.py
+++ jamyunggo/backends/telegram.py
@@ -18,13 +18,15 @@ def notify(module_name, title, text=None, url=None):
     if url:
         text_msg += '<a href="%s">%s</a>\n' % (url, url.replace("<", "&lt").replace(">", "&gt").replace("&", "&amp"))
     text_msg += bleach.clean(text, tags=['a','b','i','strong','code','pre'], strip=True)
-    soup = BeautifulSoup(text, 'html.parser')
-    for img in soup.find_all("img"):
-            img_urls.append(img["src"])
+    
+    # TODO: Fix photo timeout bug
+    #soup = BeautifulSoup(text, 'html.parser')
+    #for img in soup.find_all("img"):
+    #        img_urls.append(img["src"])
 
     for chat_id in CHAT_SET:
         BOT.send_message(chat_id, text_msg, parse_mode="HTML")
-        for img_url in img_urls:
-            BOT.send_photo(chat_id, img_url,  disable_notification=True, timeout=60)
+    #    for img_url in img_urls:
+    #        BOT.send_photo(chat_id, img_url,  disable_notification=True, timeout=60)
 
     return True
