93ffbee7b36216b11429cd9d3c8305167e8d3b43
Wonsup Yoon <pusnow@me.com>
2018-09-30 17:17:51 +0900

    fix

--- pages/hibrain.py
+++ pages/hibrain.py
@@ -7,7 +7,7 @@ BACKENDS = ["telegram"]
 
 NAME = "Hibrain 연구비지원"
 URL = "https://www.hibrain.net/research/researches/34/recruitments/108/recruits?sortType=AD&displayType=TIT&listType=ING&limit=25&siteid=1"
-FIND_ALL_ARGS = {"class_": "listTypeRecruit"}
+FIND_ALL_ARGS = {"class_": "sortRoot"}
 PARAM = "JMG_PATH"
 
 HEADERS = {}
