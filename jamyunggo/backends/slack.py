7d1587639e8be152099db01d59247cb14c1a783c
Wonsup Yoon <pusnow@kaist.ac.kr>
2020-04-19 12:03:21 +0000

    remove replaces

--- jamyunggo/backends/slack.py
+++ jamyunggo/backends/slack.py
@@ -16,14 +16,11 @@ def notify(module_name, title, text=None, url=None, name=None):
     if not config["SLACK"]["WEBHOOK"]:
         return False
     if name:
-        text_msg = "[%s]%s\n" % (name, title.replace("<", "&lt").replace(
-            ">", "&gt").replace("&", "&amp"))
+        text_msg = "[%s]%s\n" % (name, title)
     else:
-        text_msg = "%s\n" % title.replace("<", "&lt").replace(
-            ">", "&gt").replace("&", "&amp")
+        text_msg = "%s\n" % title
     if url:
-        text_msg += '%s\n' % url.replace("<", "&lt").replace(
-            ">", "&gt").replace("&", "&amp")
+        text_msg += '%s\n' % url
 
     webhook_url = config["SLACK"]["WEBHOOK"]
     session.post(webhook_url, json={
